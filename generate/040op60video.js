7409fa899868a1c6a6c2ce01176a019549918a9f
Michiel Buddingh <michiel@michielbuddingh.net>
2021-10-09 13:28:39 +0200

    WIP

---
+++ generate/040op60video.js
@@ -0,0 +1,88 @@
+"use strict";
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsed');
+
+(() => {
+    let tag = document.createElement('script');
+//    tag.id = 'iframe-demo';
+    tag.src = 'https://www.youtube.com/iframe_api';
+    let firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+})();
+
+
+let videoElement = document.getElementById('existing-iframe-example');
+
+history.replaceState({"geroteerd": false}, "Zet 'm op 60");
+let oldRoot = null;
+
+function _04060roteerVideo(el) {
+    if (!el.classList.contains("_040op60rotated")) {
+	oldRoot = el.parentNode;
+	let body = document.querySelector("body");
+	body.insertBefore(el, body.childNodes[0]);
+	history.pushState({"geroteerd": true}, "Geroteerde video");
+	el.classList.add("_040op60rotated");
+    }
+}
+
+function _04060normaleVideo(el) {
+    if (el.classList.contains("_040op60rotated")) {
+	if (oldRoot != null) {
+	    oldRoot.appendChild(el);
+	}
+	el.classList.remove("_040op60rotated")
+    }
+    if (history.state && history.state.geroteerd) {
+	history.go(-1);
+    }
+}
+
+window.addEventListener('popstate', function(event) {
+    console.log("OnPopState", event);
+    if (history.state) {
+	if (history.state.geroteerd) {
+	    _04060roteerVideo(videoElement);
+	} else if (history.state.geroteerd === false) {
+	    _04060normaleVideo(videoElement);
+	}
+    }
+})
+
+function rotate(playerStatus) {
+    let videoElement = document.getElementById('existing-iframe-example');
+
+    // playing // paused // buffering
+    if (playerStatus == 1 || playerStatus === 2 || playerStatus === 3) {
+	_04060roteerVideo(videoElement);
+    } else {
+	_04060normaleVideo(videoElement);
+    }
+}
+
+function onPlayerStateChange(event) {
+    console.log("playerstatechange");
+    rotate(event.data);
+}
+
+function onPlayerReady(event) {
+    console.log("onplayerready");
+    player.addEventListener("onStateChange", onPlayerStateChange);
+}
+
+var player;
+function onYouTubeIframeAPIReady() {
+    console.log("YoutubeIframeAPIReady");
+    player = new YT.Player('existing-iframe-example', {
+        events: {
+	    'onReady' : onPlayerReady,
+	    'onStateChange': onPlayerStateChange
+        }
+    });
+    console.log("YoutubeIframeAPIFinished", player, "foo");
+}
+
+
+console.log("End of script");
+//});
---
+++ generate/generate.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+	"io/ioutil"
+	"strings"
+	"log"
+	"html/template"
+	"os"
+	"path"
+)
+
+func textFromPath(path string) (string, error) {
+	b, err := ioutil.ReadFile(path)
+	if err == nil {
+		s := string(b)
+		return strings.TrimSpace(s), nil
+	}
+	return "", err
+}
+
+
+type Ketel struct {
+	Merk *Merk
+	Ketelnaam string
+	Stappenplan string
+	Ketelherkenning string
+	Video string
+	Path string
+	Filename string
+}
+
+
+type Merk struct {
+	Naam string
+	Logo string
+	Ketels []*Ketel
+	Path string
+	Filename string
+}
+
+
+func main() {
+	var err error
+
+
+
+	var merken []*Merk
+	var ketels []*Ketel
+	var cur *Ketel
+	var curMerk *Merk
+
+	err = filepath.Walk("../ketels", func(filepath string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("prevent panic by handling failure accessing a path %q: %v\n", filepath, err)
+			return err
+		}
+
+		if info.IsDir() {
+			dir, path := path.Split(filepath)
+			if dir == "../" {
+			} else if dir == "../ketels/" {
+				// Dit is een merk
+				if curMerk != nil {
+					merken = append(merken, curMerk)
+				}
+				curMerk = &Merk{
+					Filename: path,
+					Path:filepath}
+			} else {
+				if cur != nil {
+					ketels = append(ketels, cur)
+				}
+				cur = &Ketel{
+					Merk: curMerk,
+					Ketelnaam: "ketelnaam",
+					Filename: path,
+					Path:filepath}
+
+				curMerk.Ketels = append(curMerk.Ketels, cur)
+			}
+		}
+
+		if info.Name() == "merknaam.txt" {
+			curMerk.Naam, _ = textFromPath(filepath)
+		}
+
+		if info.Name() == "ketel.png" {
+			cur.Ketelherkenning = filepath
+		}
+
+		if info.Name() == "ketel.png" {
+			cur.Ketelherkenning = filepath
+		}
+		if info.Name() == "naam.txt" {
+			cur.Ketelnaam, _ = textFromPath(filepath)
+		}
+
+		if info.Name() == "handleiding.png" {
+			cur.Stappenplan = filepath
+		}
+		if info.Name() == "video" {
+			cur.Video, _ = textFromPath(filepath)
+		}
+
+		return nil
+	})
+
+
+	if err != nil {
+		fmt.Printf("error walking the path %v\n", err)
+		return
+	}
+
+	for _, k := range ketels {
+		fmt.Printf("%s, %#v\n", k.Ketelnaam, k.Merk)
+	}
+
+	templ, err := template.ParseFiles("ketel-template.html")
+	if err != nil {
+		log.Fatalf("Could not open template")
+	}
+
+	for ki := range ketels {
+		p := "out/out/" + ketels[ki].Path
+		err := os.MkdirAll(p, fs.ModePerm)
+		if err != nil {
+			log.Fatalf("Error creating directory")
+		}
+		f, err := os.Create(p + "/" + ketels[ki].Filename + ".html")
+		if err == nil {
+			templ.Execute(f, ketels[ki])
+		}
+		f.Close()
+	}
+
+	merktempl, err := template.ParseFiles("merk-template.html")
+	if err != nil {
+		log.Fatalf("Could not open merk template")
+	}
+
+	for mi := range merken {
+		p := "out/out/" + merken[mi].Path
+		err := os.MkdirAll(p, fs.ModePerm)
+		if err != nil {
+			log.Fatalf("Error creating directory")
+		}
+		f, err := os.Create(p + "/" + merken[mi].Filename + ".html")
+		if err == nil {
+			merktempl.Execute(f, merken[mi])
+		}
+		f.Close()
+	}
+
+
+}
